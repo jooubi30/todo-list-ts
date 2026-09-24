31130fc704e9d4bc3cffc958cef9686c2665dcf3
Bogdan <bogy2015@mail.ru>
2026-09-24 20:15:38 +0400

    chore: Ипортировал тип, Объявил переменные для хранения хардкор значений, Добавил деструктуризацию для аргументов функции

--- src/components/StatCard/StatCard.ts
+++ src/components/StatCard/StatCard.ts
@@ -1,24 +1,24 @@
-type StatCardType = 'default' | 'active' | 'completed';
+import type { StatCardProps } from "./StatCard.types";
 
-interface StatCardProps {
-  count: number;
-  status: string;
-  type?: StatCardType;
-}
+const BASE_CLASSNAME = 'stat-card';
+const BASE_CLASSNAME_COUNT = BASE_CLASSNAME + '__count';
+const BASE_CLASSNAME_STATUS = BASE_CLASSNAME + '__status';
 
-export function createStatCard({count, status, type = 'default'}: StatCardProps): HTMLElement {
+export function createStatCard(props: StatCardProps): HTMLElement {
+  const {count, status, type = 'default'} = props;
+
   const card = document.createElement('div');
-  card.className = 'stat-card'
+  card.className = BASE_CLASSNAME;
 
   const countEl = document.createElement('div');
-  countEl.className = 'stat-card__count'
+  countEl.className = BASE_CLASSNAME_COUNT
   if (type !== 'default') {
-    countEl.classList.add(`stat-card__count--${type}`);
+    countEl.classList.add(`${BASE_CLASSNAME_COUNT}--${type}`);
   }
   countEl.textContent = String(count);
 
   const statusEl = document.createElement('div');
-  statusEl.className = 'stat-card__status'
+  statusEl.className = BASE_CLASSNAME_STATUS;
   statusEl.textContent = status;
 
   card.append(countEl, statusEl);
